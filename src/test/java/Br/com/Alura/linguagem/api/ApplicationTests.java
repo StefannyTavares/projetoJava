120451c65c7e17fa8bd393b686e9e64dbdfccdc0
Stefanny Tavares <stefanny.tavares@hotmail.com>
2022-07-27 16:43:09 -0300

    first commit

---
+++ src/test/java/Br/com/Alura/linguagem/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Br.com.Alura.linguagem.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
